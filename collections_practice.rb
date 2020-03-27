168633482f2c403c3126ce25a4a86f65a82120c3
Christopher Reinken <christopherreinken@gmail.com>
2020-03-27 17:37:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,9 @@
 def sort_array_asc(integers)
-Build a method sort_array_asc that takes in an array of integers and returns a copy of the array with the integers in ascending order.
+
 end
 
 def sort_array_desc(integers)
-  Build a method sort_array_desc that takes in an array of integers and returns a copy of the array with the integers in descending order. Remember that .sort takes a block in which you can specify how you want your array sorted.
+  
 end
 
 def sort_array_char_count(array)
